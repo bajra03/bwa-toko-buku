0f144cce6fb0b12438a1b72d0749cfa3be879ddf
bajra <bajra.idabagus@karmagroup.com>
2022-04-25 16:50:46 +0800

    update book and delete book

--- app/api/books/controller.js
+++ app/api/books/controller.js
@@ -34,4 +34,139 @@ module.exports = {
       next(err);
     }
   },
+
+  createBooks: async (req, res, next) => {
+    try {
+      let user = req.user.id;
+      const {
+        title,
+        category,
+        author,
+        cover,
+        publish,
+        price,
+        stock
+      } = req.body;
+
+      const checkCategory = await Category.findOne({
+        where: {
+          id: category,
+          user: user
+        }
+      });
+
+      if (!checkCategory) {
+        return res.status(404).json({
+          message: "Category not found"
+        });
+      }
+
+      const books = await Book.create({
+        title,
+        category,
+        author,
+        cover,
+        publish,
+        price,
+        stock,
+        user: user
+      });
+
+      res.status(201).json({
+        message: "Book has been created!",
+        data: books
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  updateBooks: async (req, res, next) => {
+    try {
+      let user = req.user.id;
+      const { id } = req.params;
+      const {
+        title,
+        category,
+        author,
+        cover,
+        publish,
+        price,
+        stock
+      } = req.body;
+
+      const checkCategory = await Category.findOne({
+        where: {
+          id: category,
+          user: user
+        }
+      });
+
+      if (!checkCategory) {
+        return res.status(404).json({
+          message: "Category not found"
+        });
+      }
+
+      const checkBook = await Book.findOne({
+        where: {
+          id: id
+        }
+      });
+
+      if (!checkBook) {
+        res.status(404).json({
+          message: "Book now found"
+        });
+      }
+
+
+      const books = await checkBook.update({
+        title,
+        category,
+        author,
+        cover,
+        publish,
+        price,
+        stock,
+        user: user
+      });
+
+      res.status(200).json({
+        message: "Book has been updated!",
+        data: books
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  deleteBooks: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      let user = req.user.id;
+
+      const checkBook = await Book.findOne({
+        where: {
+          id: id,
+          user: user
+        }
+      });
+
+      if (!checkBook) {
+        return res.status(404).json({
+          message: "ID Book not found!"
+        });
+      }
+
+      const book = await checkBook.destroy();
+
+      res.status(200).json({
+        message: "Book has been deleted",
+        data: book
+      })
+    } catch (err) {
+      next(err);
+    }
+  }
 };
