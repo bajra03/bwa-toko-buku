be85faf91bc1ec3a1d1cc0a60ef099b844e9f143
bajra <bajra.idabagus@karmagroup.com>
2022-04-25 11:34:01 +0800

    update comment

--- app/middlewares/multer.js
+++ app/middlewares/multer.js
@@ -1,16 +1,27 @@
+// this middleware is for handle file/image upload
 const multer = require('multer');
 
+// set the destinations folder to store the image and set the image name
 const storage = multer.diskStorage({
+  // set the destination where the file/image will be stored
   destination: function (req, file, cb) {
     cb(null, 'public/uploads/');
   },
+  // set the file name, give the time format in the front of file name
+  // to avoid the same file name
   filename: function (req, file, cb) {
     cb(null, new Date().getTime() + '-' + file.originalname);
   }
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
+  // set the filter of the type that can be upload
+  // other than that type will return false and get the error message
+  if (
+    file.mimetype === 'image/jpeg' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/png'
+  ) {
     cb(null, true);
   } else {
     // reject the file
@@ -18,6 +29,7 @@ const fileFilter = (req, file, cb) => {
   }
 }
 
+// set the multer object property
 const uploadMiddleware = multer({
   storage: storage,
   fileFilter: fileFilter
