58bd4b97b0a04422d17f07d8942ab47c4e6552a9
bajra03 <putu.bajra@gmail.com>
2022-05-07 10:43:12 +0800

    add comment

--- app/api/auth/router.js
+++ app/api/auth/router.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.post('/auth/signin', signIn);
 router.post('/auth/signup', signUp);
 
+// get auth router
 router.get('/auth/signin', (req, res, next) => {
   res.status(200).json({
     message: 'Signin GET'
